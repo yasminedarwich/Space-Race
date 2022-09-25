986c35f5c244591d9568d2841ed1cfb66d23893f
Yasmine <95974713+yasminedarwich@users.noreply.github.com>
2022-09-25 16:24:05 +0300

    Update 02_🚀_Rocket Leaders.py

--- SpaceRace/pages/02_🚀_Rocket Leaders.py
+++ SpaceRace/pages/02_🚀_Rocket Leaders.py
@@ -6,7 +6,6 @@ import seaborn as sns
 color = sns.color_palette()
 
 import plotly.offline as py
-py.init_notebook_mode(connected=True)
 import plotly.graph_objs as go
 import plotly.tools as tls
 import plotly.express as px
